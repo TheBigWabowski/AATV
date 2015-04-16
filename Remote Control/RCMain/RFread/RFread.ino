0e7ed08a7415c3fe4682c5456eccf86f2b2069ad
TheBigWabowski <onealodoma@georgeschool.org>
2015-04-16 13:03:41 -0400

    commit

--- Remote Control/RCMain/RFread/RFread.ino
+++ Remote Control/RCMain/RFread/RFread.ino
@@ -1,6 +1,3 @@
-
-// Copyright Ephraim Benson 2015
-
 const byte rightXPin = 0;
 const byte rightYPin = 1;
 const byte killSwitchPin = 3;
@@ -26,21 +23,16 @@ void setup() {
 
   pinMode(servoLeft, OUTPUT);
   pinMode(servoRight, OUTPUT);
-<<<<<<< HEAD
   
   pinMode(driverPower, OUTPUT);
   pinMode(driverPolarity = OUTPUT);
-=======
-  pinMode(servoSteering, OUTPUT);
-  pinMode(driverPin, OUTPUT);
->>>>>>> origin/master
 
 
   pinMode(LEDPin, OUTPUT);
 }
 
 void loop() {
-  
+  main();
 }
 
 
@@ -48,10 +40,8 @@ void main(){
   int valueX = pulseIn(rightXPin, HIGH);
   int valueY = pulseIn(rightYPin, HIGH);
   int valueKillSwitch = pulseIn(killSwitchPin, HIGH);
-  int valueX_remapped = map(valueX, 1000, 1800, 0, 2);
 
   bool shouldRun = map(valueKillSwitch, 1000, 1800, 0, 1);
-<<<<<<< HEAD
   if (shouldRun) {                                   //Test for Kill Switch
     yAxis = map(rightYPin, 1000, 1800, 0, 2);
     if (yAxis = 0){                                     //Test for Reverse
@@ -64,29 +54,6 @@ void main(){
      driverForward();
     }
     else {digitalWrite(LEDPin, HIGH)};
-=======
-  if (shouldRun) {
-
-  	// Y Movement
-    ServoMove(servoLeft, 2000- valueY + 1000);
-    ServoMove(servoRight, valueY);
-
-    // X Movement
-    if (valueX_remapped == 0) {
-    	ServoMove(servoSteering, 1000);
-    	Serial.println("left");
-    } else if (valueX_remapped == 2) {
-    	Serial.println("right");
-    	ServoMove(servoSteering, 2000);
-    } else {
-    	Serial.println("forward");
-    	ServoMove(servoSteering, 1500);
-    }
-    
-    practiceDriverForward();
-
-    delay(20);
->>>>>>> origin/master
   }
     xAxis = map(rightXPin, 1000, 1800, 1000, 2000);
      analogWrite(steeringPin, xAxis);
