18fde37cd21aae4ecdf0a0b1e0fb268c83ac3b68
TheBigWabowski <onealodoma@georgeschool.org>
2015-05-12 13:53:48 -0400

    Revert "Nothing Much"
    This reverts commit a6c93d63109d3bd0d005703316f6938cf205bd24.

--- Remote Control/RCMain/RCMain.ino
+++ Remote Control/RCMain/RCMain.ino
@@ -1,13 +1,10 @@
- //Semi-Autonomous All Terrain Vehicle
+//Semi-Autonomous All Terrain Vehicle
 //Room Four Robotics 2015-04-1
 
-int LEDPin = 13;
-
-
 void setup(){
-  pinMode(LEDPin, OUTPUT);
-  Serial.begin(9600); 
+ Serial.begin(9600); 
 }
 
-void loop(){ 
+void loop(){
+  
 }
